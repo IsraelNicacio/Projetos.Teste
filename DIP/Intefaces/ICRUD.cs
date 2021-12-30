8a993d532a26594172ad20adefa5c1ecf6a0dad9
Israel <idias@infofisco.com.br>
2021-12-30 12:40:33 -0300

    Adicionado DIP para Banco

---
+++ DIP/Intefaces/ICRUD.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIP.Intefaces
+{
+    public interface ICRUD
+    {
+        public void ExecuteNonQuery(SqlCommand command, IEnumerable<SqlParameter> sqlParameters);
+        public object ExecuteScalar(SqlCommand command, IEnumerable<SqlParameter> sqlParameters);
+        public DataTable ExecuteToDataTable(SqlCommand command, IEnumerable<SqlParameter> sqlParameters);
+    }
+}
